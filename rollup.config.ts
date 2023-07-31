f306a4f6924b80a6d0718aa47cb73a4ae9f66250
sarawinter <sarawinter79@gmail.com>
2023-07-31 11:13:42 +0200

    updated rollup config to use babel

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postCSS from "rollup-plugin-postcss";
-import babel from "@rollup/plugin-babel"
+import babel from "@rollup/plugin-babel";
 
 import pkg from "./package.json" assert { type: "json" };
 import sucrase from "@rollup/plugin-sucrase";
@@ -22,7 +22,11 @@ export default {
   external: [...Object.keys(pkg.peerDependencies || {})],
   plugins: [
     babel({
-      presets: ["@babel/preset-react"],
+      exclude: "node_modules/**",
+      presets: [
+        "@babel/preset-env",
+        ["@babel/preset-react", { runtime: "automatic" }],
+      ],
     }),
     nodeResolve(),
     commonjs(),
