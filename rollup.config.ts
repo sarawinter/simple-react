8df2307a40999234b3921be3ff8e730bc20f4fcd
sarawinter <sarawinter79@gmail.com>
2023-07-31 11:01:12 +0200

    installed babel

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postCSS from "rollup-plugin-postcss";
+import babel from "@rollup/plugin-babel"
 
 import pkg from "./package.json" assert { type: "json" };
 import sucrase from "@rollup/plugin-sucrase";
@@ -20,6 +21,9 @@ export default {
   ],
   external: [...Object.keys(pkg.peerDependencies || {})],
   plugins: [
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
     nodeResolve(),
     commonjs(),
     typescript(),
